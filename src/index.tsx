c25c95efc73e489cbb25cf93870639d80c3177b5
Slaven Bacic <bacic.slaven@gmail.com>
2021-03-27 10:22:04 +0100

    refactor: create single file for all exports

---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+import { useWizard, WizardContext } from './hook';
+import { Wizard, WizardRenderer, Optional } from './components';
+export { useWizard, WizardContext, WizardRenderer, Wizard, Optional };
